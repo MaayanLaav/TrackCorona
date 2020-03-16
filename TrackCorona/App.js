ffe9e139a415d269a8eb52fa22f911f13918b401
Guy Zion <guyzion7@gmail.com>
2020-03-16 13:11:20 +0200

    navigator almost complete

--- TrackCorona/App.js
+++ TrackCorona/App.js
@@ -1,38 +1,23 @@
 import React, { Component } from 'react';
 import HomeScreen from './src/screens/HomeScreen';
-import MapScreen from './src/screens/MapScreen';
-import HelpScreen from './src/screens/HelpScreen';
-import SettingsScreen from './src/screens/SettingsScreen';
-import { createAppContainer } from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
-const bottomTabNavigator = createBottomTabNavigator(
-  {
-    Home:  {
-      screen: HomeScreen,    
-    },
-    Map: {
-      screen: MapScreen,
-    },
-    Help: {
-      screen: HelpScreen,
-    },
-    Settings: {
-      screen: SettingsScreen,
-    },
-  },
-  {
-    initialRouteName: 'Home',
-    tabBarOptions: {
-      activeTintColor: '#eb6e3d'
-    }
-  }
-);
-const AppContainer = createAppContainer(bottomTabNavigator);
+import Navigator from './src/components/Navigator';
+import { Text, View, TouchableOpacity, Alert } from 'react-native';
+
 
 export default class App extends Component {
   render(){
     return(
-      <AppContainer/>
+      <View style={{flex:1}}>
+        <View style={{flex:1}}>
+        </View>
+        <View style={{flex:8}}>
+          <HomeScreen/>
+        </View>
+        <View style={{flex:1}}>
+          <Navigator/>
+        </View>
+      </View>
+        
     );
   }
 }
