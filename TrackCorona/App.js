214fade64720feac391564fafb5313a1effab51c
MaayanLaav <51079514+MaayanLaav@users.noreply.github.com>
2020-03-16 10:51:42 +0400

    comit

--- TrackCorona/App.js
+++ TrackCorona/App.js
@@ -5,7 +5,6 @@ import HelpScreen from './src/screens/HelpScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
 import { createAppContainer } from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
-const Tab = createBottomTabNavigator();
 const bottomTabNavigator = createBottomTabNavigator(
   {
     Home:  {
@@ -33,10 +32,6 @@ const AppContainer = createAppContainer(bottomTabNavigator);
 export default class App extends Component {
   render(){
     return(
-      <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Help" component={HelpScreen} />
-    </Tab.Navigator>
       <AppContainer/>
     );
   }
