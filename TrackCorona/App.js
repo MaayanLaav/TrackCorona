53b10d7551de5cbff9bfefa003a8e7257306ae6b
MaayanLaav <51079514+MaayanLaav@users.noreply.github.com>
2020-03-21 18:31:31 +0200

    commit

--- TrackCorona/App.js
+++ TrackCorona/App.js
@@ -16,6 +16,7 @@ export default class App extends Component {
     status: 0,
     dateOfDiagnosis: null,
     dateOfFirstExposure: null,
+    infectedPoint:null
   }
   constructor(props) {
     super(props);
@@ -29,10 +30,16 @@ export default class App extends Component {
     let response = await fetch(serverUrl + 'getUserDetails/' + this.state.userName.replace(/\s/g, ''));
     let data = await response.json();
     if (data && data.recordset && data.recordset[0]) {
-      this.setState({ status: data.recordset[0].Status, dateOfDiagnosis: data.recordset[0].DateOfDiagnosis, dateOfFirstExposure: data.recordset[0].DateOfFirstExposure });
+      this.setState({ status: data.recordset[0].Status, dateOfDiagnosis: data.recordset[0].DateOfDiagnosis, dateOfFirstExposure: data.recordset[0].DateOfFirstExposure,infectedPoint:data.recordset[0].infectedPoint});
     }
   }
   render() {
+    let infectedLat = null;
+    let infectedLon = null;
+    if (this.state.infectedPoint){
+      infectedLat = this.state.infectedPoint.Latitude;
+      infectedLon = this.state.infectedPoint.Longitude;
+    }
     return (
       <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#333333' }}>
         {/* Header Title */}
@@ -47,7 +54,7 @@ export default class App extends Component {
           {/* <MapHealthMinistry/> */}
           <View style={{ height: '90%', width: 350, justifyContent: 'center', alignItems: "center", backgroundColor: 'white', borderRadius: 25 }}>
             <View style={{ width: 320, height: '90%' }}>
-              <FindMe />
+              <FindMe infectedLat = {infectedLat} infectedLon= {infectedLon}  />
             </View>
           </View>
         </View>
--- TrackCorona/src/components/PakarMap.js
+++ TrackCorona/src/components/PakarMap.js
@@ -6,33 +6,50 @@ export default class App extends Component {
     state = {
         region: null,
         latitude:32.064639,
-        longitude: 34.773554
+        longitude: 34.773554,
+        typeOfPoint: 'myLocation',
+        counter: 0
       
       }
       constructor(props){
           super(props);
+          this.onRegionChange = this.onRegionChange.bind(this);
+          this.state.latitude = this.props.latitude  || 32.064639 ;
+          this.state.longitude = this.props.longitude || 34.773554  ;
+          if (this.props.latitude){
+            this.state.region = {latitude:this.state.latitude,longitude:this.state.longitude, latitudeDelta: 0.01, longitudeDelta: 0.01};
+          }
+          this.state.typeOfPoint = this.props.typeOfPoint;
       }
       componentDidUpdate(prevProps) {
         // Typical usage (don't forget to compare props):
-        if (this.props.latitude !== prevProps.latitude || this.props.longitude !== prevProps.longitude  ) {
-          this.setState({ latitude: this.props.latitude, longitude: this.props.longitude })
-          if (this.state.region == null){
-               let reg = { latitude: this.props.latitude, longitude: this.props.longitude, latitudeDelta: 0.01, longitudeDelta: 0.01 }
-               this.setState({region:reg});
+        if (this.props.latitude != prevProps.latitude || this.props.longitude != prevProps.longitude  ) {
+          var newState = { latitude: this.props.latitude, longitude: this.props.longitude,typeOfPoint:this.props.typeOfPoint }
+          if (this.state.region == null || this.props.typeOfPoint != prevProps.typeOfPoint){
+            console.log("region should change");
+            newState.region = { latitude: this.props.latitude, longitude: this.props.longitude, latitudeDelta: 0.01, longitudeDelta: 0.01 }
           }
+          
+          this.setState(newState);
         }
       }
-
+      onRegionChange = function(region) {
+          this.setState({region: region});
+          console.log("region set")
+      }
     render (){
+      let titleOfPoint = this.state.typeOfPoint == 'myLocation' ? 'מיקומך הנוכחי' : 'נקודת החשיפה';
+      let colorOfPoint = this.state.typeOfPoint == 'myLocation' ? 'blue' : 'red';
         var markers = [ {
-            title: 'מיקומך הנוכחי',
+            title: titleOfPoint,
             coordinates: { latitude: this.state.latitude, longitude: this.state.longitude },
-            tracksInfoWindowChanges:true
+            tracksInfoWindowChanges:true,
+            pinColor: colorOfPoint
           }]
           
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}  initialRegion={this.state.region}>
+      <MapView style={styles.map} region={this.state.region} onRegionChangeComplete={this.onRegionChange}>
         {markers.map(marker => (
           <MapView.Marker
             coordinate={marker.coordinates}
